2d5dbcb0be46a289fd9be72c54ef8161d1b74b78
praneeth <praneeth@mit.edu>
2022-03-18 13:15:26 -0400

    refactor videoviewer from video to gui

--- pntools/gui.py
+++ pntools/gui.py
@@ -6,14 +6,32 @@ Classes:
     PlotBrowser - Scroll through an array of complex data where a plotting function is defined for each element
 """
 import io
-from PySide2.QtGui import QClipboard, QImage
+import os
+from datetime import timedelta
+
+import ffmpeg
 import matplotlib as mpl
+from PySide2.QtGui import QClipboard, QImage
 from matplotlib import pyplot as plt
+from decord import VideoReader
 
 from pntools import sampled
 
 
 class GenericBrowser:
+    """
+    Generic class to browse data. Meant to be extended before use.
+
+    Default Navigation (arrow keys):
+        right       - forward one frame
+        left        - back one frame
+        up          - forward 10 frames
+        down        - back 10 frames
+        shift+left  - first frame
+        shift+right - last frame
+        shift+up    - forward nframes/20 frames
+        shift+down  - back nframes/20 frames
+    """
     def __init__(self, figure_handle=None):
         if figure_handle is None:
             figure_handle = plt.figure()
@@ -22,11 +40,17 @@ class GenericBrowser:
         self._keypressdict = {}
         self._bindings_removed = {}
         
+        # tracking variable
+        self._current_idx = 0
+
         # for cleanup
         self.cid = []
         self.cid.append(self._fig.canvas.mpl_connect('key_press_event', self))
         self.cid.append(self._fig.canvas.mpl_connect('close_event', self))
     
+    def update(self):
+        pass # inherited classes are expected to implement an update function!
+
     def mpl_remove_bindings(self, key_list):
         """If the existing key is bound to something in matplotlib, then remove it"""
         for key in key_list:
@@ -58,6 +82,10 @@ class GenericBrowser:
                 mpl.rcParams[param_name].append(key) # param names: keymap.back, keymap.forward)
             self._bindings_removed[param_name] = {}
 
+    def __len__(self):
+        if hasattr(self, 'data'): # otherwise returns None
+            return len(self.data)
+
     # Event responses - useful to pair with add_key_binding
     # These capabilities can be assigned to different key bindings
     def add_key_binding(self, key_name, on_press_function):
@@ -71,15 +99,38 @@ class GenericBrowser:
     def set_default_keybindings(self):
         self.add_key_binding('left', self.decrement)
         self.add_key_binding('right', self.increment)
+        self.add_key_binding('up', (lambda s: s.increment(step=10)).__get__(self))
+        self.add_key_binding('down', (lambda s: s.decrement(step=10)).__get__(self))
+        self.add_key_binding('shift+left', self.increment_frac)
+        self.add_key_binding('shift+right', self.decrement_frac)
+        self.add_key_binding('shift+up', self.go_to_start)
+        self.add_key_binding('shift+down', self.go_to_end)
         self.add_key_binding('ctrl+c', self.copy_to_clipboard)
     
-    def increment(self):
-        self._current_idx = min(self._current_idx+1, len(self.plot_data)-1)
-        self.update_figure()
+    def increment(self, step=1):
+        self._current_idx = min(self._current_idx+step, len(self)-1)
+        self.update()
 
-    def decrement(self):
-        self._current_idx = max(self._current_idx-1, 0)
-        self.update_figure()
+    def decrement(self, step=1):
+        self._current_idx = max(self._current_idx-step, 0)
+        self.update()
+    
+    def go_to_start(self): # default: shift+left
+        self._current_idx = 0
+        self.update()
+    
+    def go_to_end(self):
+        self._current_idx = len(self)-1
+        self.update()
+    
+    def increment_frac(self, n_steps=20):
+        # browse entire dataset in n_steps
+        self._current_idx = min(self._current_idx+int(len(self)/n_steps), len(self)-1)
+        self.update()
+    
+    def decrement_frac(self, n_steps=20):
+        self._current_idx = max(self._current_idx-int(len(self)/n_steps), 0)
+        self.update()
     
     def copy_to_clipboard(self):
         buf = io.BytesIO()
@@ -105,22 +156,19 @@ class PlotBrowser(GenericBrowser):
         # setup
         super().__init__(figure_handle)
 
-        self.plot_data = plot_data # list where each element serves as input to plot_func
+        self.data = plot_data # list where each element serves as input to plot_func
         self.plot_func = plot_func
         self.plot_kwargs = plot_kwargs
-
-        # tracking variable
-        self._current_idx = 0
 
         # initialize
         self.set_default_keybindings()
         plt.show(block=False)
-        self.update_figure()
+        self.update()
         
     def get_current_data(self):
-        return self.plot_data[self._current_idx]
+        return self.data[self._current_idx]
 
-    def update_figure(self):
+    def update(self):
         self._fig.clear()
         self.plot_func(self.get_current_data(), self._fig, **self.plot_kwargs)
         plt.draw()
@@ -140,12 +188,9 @@ class SignalBrowser(GenericBrowser):
         else:
             self._plot = self._ax.plot(this_data)[0]
 
-        self._current_idx = 0
-
-        self.n_plots = len(plot_data)
-        self.plot_data = plot_data
+        self.data = plot_data
         if titlefunc is None:
-            if hasattr(self.plot_data[0], 'name'):
+            if hasattr(self.data[0], 'name'):
                 self.titlefunc=lambda s: f'{s.plot_data[s._current_idx].name}'
             else:
                 self.titlefunc = lambda s: f'Plot number {s._current_idx}'
@@ -155,13 +200,55 @@ class SignalBrowser(GenericBrowser):
         # initialize
         self.set_default_keybindings()
         plt.show(block=False)
-        self.update_figure()
-
-    def update_figure(self):
-        this_data = self.plot_data[self._current_idx]
+        self.update()
+    
+    def update(self):
+        this_data = self.data[self._current_idx]
         if isinstance(this_data, sampled.Data):
             self._plot.set_data(this_data.t, this_data())
         else:
             self._plot.set_ydata()
         self._ax.set_title(self.titlefunc(self))
         plt.draw()
+
+CLIP_FOLDER = 'C:\\data\\_clipcollection'
+
+class VideoBrowser(GenericBrowser):
+    def __init__(self, vid_name, titlefunc=None, figure_handle=None):
+        super().__init__(figure_handle)
+
+        if not os.path.exists(vid_name): # try looking in the CLIP FOLDER
+            vid_name = os.path.join(CLIP_FOLDER, os.path.split(vid_name)[-1])
+        
+        assert os.path.exists(vid_name)
+        with open(vid_name, 'rb') as f:
+            self.data = VideoReader(f)
+        
+        self._ax = self._fig.subplots(1, 1)
+        this_data = self.data[0]
+        self._im = self._ax.imshow(this_data.asnumpy())
+
+        self.fps = self.data.get_avg_fps()
+        plt.axis('off')
+        if titlefunc is None:
+            self.titlefunc = lambda s: f'Frame {s._current_idx}/{len(s)}, {s.fps} fps, {str(timedelta(seconds=s._current_idx/s.fps))}'
+        
+        self.set_default_keybindings()
+        plt.show(block=False)
+        self.update()
+
+    def update(self):
+        self._im.set_data(self.data[self._current_idx].asnumpy())
+        self._ax.set_title(self.titlefunc(self))
+        plt.draw()
+
+    def extract_clip(self, start_frame, end_frame, fname_out=None, out_rate=None):
+        start_time = float(start_frame)/self.fps
+        end_time = float(end_frame)/self.fps
+        dur = end_time - start_time
+        if out_rate is None:
+            out_rate = self.fps
+        if fname_out is None:
+            fname_out = os.path.join(CLIP_FOLDER, os.path.splitext(self.vid_name)[0] + '_s{:.3f}_e{:.3f}.mp4'.format(start_time, end_time))
+        ffmpeg.input(self.vid_name, ss=start_time).output(fname_out, vcodec='h264_nvenc', t=dur, r=out_rate).run()
+        return fname_out
--- pntools/video.py
+++ pntools/video.py
@@ -15,55 +15,8 @@ from pytube import YouTube
 
 CLIP_FOLDER = 'C:\\data\\_clipcollection'
 
-class View(VideoReader):
-    """
-    View videos [in the future, annotate them and extract clips]
-    
-    Navigation (arrow keys):
-        right       - forward one frame
-        left        - back one frame
-        up          - forward 10 frames
-        down        - back 10 frames
-        shift+left  - first frame
-        shift+right - last frame
-        shift+up    - forward nframes/20 frames
-        shift+down  - back nframes/20 frames
-
-    Player mode:
-        Play and pause with audio
-        
-    Annotations:
-        Text annotation at frames [event markings, observations]
-        Drawing shapes
-
-    Note: I could have inherited from PlotBrowser, but re-drawing axes is not a good idea for this application
-    """
-    def __init__(self, vid_name, ax=None):
-        """
-        vid_name (str) - full path to the video file
-        ax (matplotlib AxesSubplot) - figure axis to view the video (optional)
-        """
-        if not os.path.exists(vid_name):
-            # try looking in the CLIP FOLDER
-            vid_name = os.path.join(CLIP_FOLDER, os.path.split(vid_name)[-1])
-        
-        assert os.path.exists(vid_name)
-        self.vid_name = vid_name
-        with open(vid_name, 'rb') as f:
-            super().__init__(f)
-        self._current_frame = 0
-
-        # remove default key bindings for the ones that we will use
-        self._keys_used = ['left', 'right', 'up', 'down', 'shift+left', 'shift+right', 'shift+up', 'shift+down']
-        self._bindings_removed = {}
-        for key in self._keys_used:
-            this_param_name = [k for k, v in mpl.rcParams.items() if isinstance(v, list) and key in v]
-            if this_param_name: # not an empty list
-                assert len(this_param_name) == 1
-                this_param_name = this_param_name[0]
-                mpl.rcParams[this_param_name].remove(key)
-                self._bindings_removed[this_param_name] = key
-
+class ParseAx:
+    def __init__(self, ax=None) -> None:
         if ax is None:
             self._fig, self._ax = plt.subplots()
         else:
@@ -80,73 +33,6 @@ class View(VideoReader):
                         if not any((bool(this_ax.get_images()), bool(this_ax.lines), bool(this_ax.collections))):
                             break
                     self._ax = this_ax
-            else:
-                self._ax = ax
-                self._fig = ax.figure
-        
-        self.im = self._ax.imshow(self[self._current_frame].asnumpy())
-        self.cid = self._fig.canvas.mpl_connect('key_press_event', self)
-        self.closeid = self._fig.canvas.mpl_connect('close_event', self)
-        self.nframes = len(self)
-        self.fps = self.get_avg_fps()
-        plt.axis('off')
-        self.title_string = ''
-        self.update_image()
-        # print(self.__dict__)
-        plt.show(block=False)
-    
-    def __call__(self, event):
-        # print(event.__dict__) # for debugging
-        if event.name == 'key_press_event':
-            update_flag = True
-            if event.key == 'right':
-                self._current_frame = min(self._current_frame+1, self.nframes-1)
-            elif event.key == 'left':
-                self._current_frame = max(self._current_frame-1, 0)
-            elif event.key == 'up':
-                self._current_frame = min(self._current_frame+10, self.nframes-1)
-            elif event.key == 'down':
-                self._current_frame = max(self._current_frame-10, 0)
-            elif event.key == 'shift+right':
-                self._current_frame = self.nframes-1
-            elif event.key == 'shift+left':
-                self._current_frame = 0
-            elif event.key == 'shift+up':
-                self._current_frame = min(self._current_frame+int(self.nframes/20), self.nframes-1)
-            elif event.key == 'shift+down':
-                self._current_frame = max(self._current_frame-int(self.nframes/20), 0)
-            else:
-                update_flag = False
-
-            if update_flag:
-                self.update_image()
-
-        elif event.name == 'close_event':
-            self._fig.canvas.mpl_disconnect(self.cid)
-            self._fig.canvas.mpl_disconnect(self.closeid)
-
-            # restore default bindings
-            for param_name, key in self._bindings_removed.items():
-                if key not in mpl.rcParams[param_name]:
-                    mpl.rcParams[param_name].append(key) # param names: keymap.back, keymap.forward)
-
-    def update_image(self):
-        # TIP: to use this class without showing the title, create a child class and replace the update_image function
-        self.im.set_data(self[self._current_frame].asnumpy())
-        self.title_string = 'Frame {:d}/{:d}, {:f} fps, '.format(self._current_frame, self.nframes, self.fps) + str(timedelta(seconds=self._current_frame/self.fps))
-        self._ax.set_title(self.title_string)
-        plt.draw()
-
-    def extract_clip(self, start_frame, end_frame, fname_out=None, out_rate=None):
-        start_time = float(start_frame)/self.fps
-        end_time = float(end_frame)/self.fps
-        dur = end_time - start_time
-        if out_rate is None:
-            out_rate = self.fps
-        if fname_out is None:
-            fname_out = os.path.join(CLIP_FOLDER, os.path.splitext(self.vid_name)[0] + '_s{:.3f}_e{:.3f}.mp4'.format(start_time, end_time))
-        ffmpeg.input(self.vid_name, ss=start_time).output(fname_out, vcodec='h264_nvenc', t=dur, r=out_rate).run()
-        return fname_out
     
 
 def download(url, start_time=None, end_time=None, dur=None, full_file=False):
@@ -193,8 +79,3 @@ def download(url, start_time=None, end_time=None, dur=None, full_file=False):
     ffmpeg.input(fname_in, ss=start_time).output(fname_out, vcodec='h264_nvenc', t=dur).run()
     
     return fname_out
-
-
-def demo():
-    vid_name = 'C:\\data\\_clipcollection\\59eH83HMI8E_s41.174_e42.642.mp4'
-    return View(vid_name)
