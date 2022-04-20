0d21acacda3b57342bde54aae12acf686d3f48b0
praneeth <praneeth@mit.edu>
2022-04-20 17:49:58 -0400

    refactor

--- pntools/sampled.py
+++ pntools/sampled.py
@@ -476,6 +476,28 @@ class Data: # Signal processing
     def interval(self):
         return Interval(self.t_start(), self.t_end(), sr=self.sr)
 
+    def _slice_to_interval(self, key: slice) -> Interval:
+        assert key.step is None # otherwise, the sampling rate is going to change, and could cause aliasing without proper filtering
+        # IF INTEGERS, assume indices, IF FLOAT, assume time
+        if isinstance(key.start, float) or isinstance(key.stop, float):
+            intvl_start = key.start
+            if key.start is None:
+                intvl_start = self.t_start()
+            intvl_end = key.stop
+            if key.stop is None:
+                intvl_end = self.t_end()
+        else: # if samples, do python indexing and don't include the end?
+            assert isinstance(key.start, (int, type(None))) and isinstance(key.stop, (int, type(None)))
+            if key.start is None:
+                intvl_start = self.t_start()
+            else:
+                intvl_start = self.t[sorted((0, key.start, len(self)-1))[1]] # clip to limits
+            if key.stop is None:
+                intvl_end = self.t_end()
+            else:
+                intvl_end = self.t[sorted((0, key.stop-1, len(self)-1))[1]]
+        return Interval(float(intvl_start), float(intvl_end), sr=self.sr)
+
     def take_by_interval(self, key: Interval):
         assert key.sr == self.sr
         his = self._history + [('slice', key)]
@@ -513,30 +535,9 @@ class Data: # Signal processing
             return interp1d(self.t, self._sig, axis=self.axis)(key)
 
         assert isinstance(key, (Interval, slice))
-        if isinstance(key, Interval):
-            return self.take_by_interval(key)
-        
-        # From here on, key is a slice object
-        assert key.step is None # otherwise, the sampling rate is going to change, and could cause aliasing without proper filtering
-        # IF INTEGERS, assume indices, IF FLOAT, assume time
-        if isinstance(key.start, float) or isinstance(key.stop, float):
-            intvl_start = key.start
-            if key.start is None:
-                intvl_start = self.t_start()
-            intvl_end = key.stop
-            if key.stop is None:
-                intvl_end = self.t_end()
-        else: # if samples, do python indexing and don't include the end?
-            assert isinstance(key.start, (int, type(None))) and isinstance(key.stop, (int, type(None)))
-            if key.start is None:
-                intvl_start = self.t_start()
-            else:
-                intvl_start = self.t[sorted((0, key.start, len(self)-1))[1]] # clip to limits
-            if key.stop is None:
-                intvl_end = self.t_end()
-            else:
-                intvl_end = self.t[sorted((0, key.stop-1, len(self)-1))[1]]
-        return self.take_by_interval(Interval(float(intvl_start), float(intvl_end), sr=self.sr))
+        if isinstance(key, slice):
+            key = self._slice_to_interval(key)
+        return self.take_by_interval(key)
 
     def make_running_win(self, win_size=0.25, win_inc=0.1):
         win_size_samples = (round(win_size*self.sr)//2)*2 + 1 # ensure odd number of samples
