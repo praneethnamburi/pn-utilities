089f83bbe8b65c522fe1d675f76a915564ac276a
praneeth <praneeth@mit.edu>
2023-12-29 22:00:48 -0500

    FileManager - added documentation and reduced dependency

--- pntools/__init__.py
+++ pntools/__init__.py
@@ -615,28 +615,68 @@ def file_size(file_list, units='MB'):
 
 class FileManager:
     """
-    Manage files in a project.
-    Created with working on the operator project.
-    Useful for creating datasets with data distributed across files and modalities.
+    Easy to use file search. Useful for managing files in not-so-obviously organized folders.
+
+    Usage: 
+        TL;DR
+        video_list = FileManager(r'C:\videos').add('video', '*.avi', include=['sony', 'panasonic'], exclude='canon')['video']
+
+        Long version
+        Consider the following file structure in C:\videos directory
+        C:\videos
+        -- sony
+           -- 142Camera.avi
+           -- 143Camera.avi
+           -- notes.txt
+        -- panasonic
+           -- 151Camera.avi
+           -- 152Camera.avi
+           -- notes.txt
+        -- panasonic2
+           -- 201Camera.avi
+           -- 202.mp4
+        -- canon
+           -- 51Camera.avi
+           -- 40Camera.avi
+           -- notes.txt
+        -- notes
+           -- notes1.txt
+           -- notes2.txt
+        1. Initialize the file manager
+            fm = FileManager(r'C:\videos')
+        2. Add files of different types with different inclusion and exclusion criteria
+            fm.add('video', '*Camera.avi', include=['sony', 'panasonic'], exclude='canon')
+            fm.add('video', '*.mp4', include='panasonic') # to include 202.mp4 in the panasonic2 folder
+            fm.add('canon', '*Camera.avi', include='canon')
+            fm.add('notes', 'notes*.txt')
+        3. Retrieve the file list
+            canon_videos = fm['canon']
+            other_videos = fm['video']
+        4. Generate a report for the different file types (number of files, and space occupied)
+            fm.report()
     """
-    def __init__(self, base_dir):
+    def __init__(self, base_dir, exclude_hidden=True):
         assert isinstance(base_dir, str)
         self.base_dir = os.path.realpath(base_dir)
         self._files = {}
         self._filters = {}
         self._inclusions = {}
         self._exclusions = {}
+        assert isinstance(exclude_hidden, bool)
+        self._exclude_hidden = exclude_hidden
     
-    def add(self, type_name, pattern_list, include=None, exclude=None):
+    def add(self, type_name, pattern_list, include=None, exclude=None, exclude_hidden=None):
         """
         Add a type of file with a given filter.
         e.g. fm.add('video', '*Camera*.avi')
         """
+        if exclude_hidden is None: # None means not specified. In this case, set it to the global default.
+            exclude_hidden = self._exclude_hidden
         if isinstance(pattern_list, str):
             pattern_list = [pattern_list]
         self._files[type_name] = []
         for pattern in pattern_list:
-            self._files[type_name] += find(pattern, path=self.base_dir)
+            self._files[type_name] += self.find(pattern, path=self.base_dir, exclude_hidden=exclude_hidden)
         self._filters[type_name] = pattern_list
         self._inclusions[type_name] = []
         self._exclusions[type_name] = []
@@ -697,6 +737,23 @@ class FileManager:
         for file_type, file_list in self._files.items():
             fs = sum(list(file_size(file_list, units=units).values()))
             print(str(len(file_list)) + ' ' + file_type + ' files taking up {:4.3f} '.format(fs) + units)
+    
+    @staticmethod
+    def find(pattern, path=None, exclude_hidden=True):
+        "Example: find('*.txt', '/path/to/dir')"
+        if path is None:
+            path = os.getcwd()
+        
+        # this bit is from stack overflow
+        result = []
+        for root, _, files in os.walk(path):
+            for name in files:
+                if fnmatch.fnmatch(name, pattern):
+                    result.append(os.path.join(root, name))
+
+        if exclude_hidden:
+            return [r for r in result if not (r.split(os.sep)[-1].startswith('~$') or r.split(os.sep)[-1].startswith('.'))]
+        return result
 
 
 ## Package management
